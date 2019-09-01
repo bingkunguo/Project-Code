9f12f676a2f5448e8ec3f3e7ec9c991256e61ff2
bingkunguo <44086934+bingkunguo@users.noreply.github.com>
2019-09-01 07:44:34 +0100

    Add files via upload

---
+++ kuka_handlit_training_SAC/pytorch-soft-actor-critic/plot.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+
+style.use('fivethirtyeight')
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
+def animate(i):
+      env_name = "kuka_handlit-v0"
+      nn_version =2
+      rewardFunctionVersion=1
+      graph_data = open("reward_each_episode_{}_{}_{}.csv".format(env_name,nn_version,rewardFunctionVersion),'r').read()
+      lines = graph_data.split('\n')
+      xs = []
+      ys = []
+      for line in lines:
+            if len(line)>1:
+                  x,y = line.split(',')
+                  xs.append(round(float(x)))
+                  ys.append(round(float(y)))
+      ax1.clear()
+      ax1.plot(ys, xs)
+
+ani = animation.FuncAnimation(fig, animate, interval = 1000)
+plt.show()
